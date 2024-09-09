a31dfcbffd33c98c4a1cc315fcdf7c40ba33a3b3
geo-petrini <geo.petrini@gmail.com>
2024-09-09 08:50:58 +0200

    added sqlalchemy

--- app.py
+++ app.py
@@ -2,10 +2,28 @@ from flask import Flask
 from flask import render_template
 from flask import request
 # from flask import Blueprint
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
 from routes.api import api as bp_api
 
 app = Flask(__name__)
 app.register_blueprint(bp_api,  url_prefix='/api')    
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@localhost/db_name'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#L'opzione SQLALCHEMY_TRACK_MODIFICATIONS è una configurazione per Flask-SQLAlchemy che determina se SQLAlchemy deve o meno tenere traccia delle modifiche sugli oggetti per ogni sessione.
+
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Importa i modelli
+from models import User
+
 
 @app.route('/')
 def home():
